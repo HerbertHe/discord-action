8d2ea4e98b69116d53b310aa79f6e0c7e2e101db
HerbertHe <HerbertHe@users.noreply.github.com>
2024-02-28 09:49:10 +0000

    deploy: a9f50cdeb3dd28272101845ec2fb58a192bedcfe

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # discord-action
+
 GitHub Action for Discord message
+
+## LICENSE
+
+MIT &copy; Herbert He
+
+## Reference
+
+[sebastianpopp/discord-action](https://github.com/sebastianpopp/discord-action)
--- index.js
+++ index.js
@@ -1,31 +1,8 @@
-var __create = Object.create;
-var __defProp = Object.defineProperty;
-var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
-var __getOwnPropNames = Object.getOwnPropertyNames;
-var __getProtoOf = Object.getPrototypeOf;
-var __hasOwnProp = Object.prototype.hasOwnProperty;
-var __copyProps = (to, from, except, desc) => {
-  if (from && typeof from === "object" || typeof from === "function") {
-    for (let key of __getOwnPropNames(from))
-      if (!__hasOwnProp.call(to, key) && key !== except)
-        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
-  }
-  return to;
-};
-var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
-  // If the importer is in node compatibility mode or this is not an ESM
-  // file that has been converted to a CommonJS file using a Babel-
-  // compatible transform (i.e. "__esModule" has not been set), then set
-  // "default" to the CommonJS "module.exports" for node compatibility.
-  isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
-  mod
-));
-
 // src/index.ts
-var import_core = __toESM(require("@actions/core"));
+import core from "@actions/core";
 try {
-  const webhook = import_core.default.getInput("webhook");
-  const message = import_core.default.getInput("message");
+  const webhook = core.getInput("webhook");
+  const message = core.getInput("message");
   fetch(webhook, {
     method: "POST",
     headers: {
@@ -36,5 +13,5 @@ try {
     })
   });
 } catch (err) {
-  import_core.default.setFailed(err.message);
+  core.setFailed(err.message);
 }
